f6293aecefde8a89906dfc723e89e1135e85f9ef
kaiolae <jallafsen@gmail.com>
2018-12-03 14:22:42 +0100

    Adjusted generate-rnn-data to handle larger data amounts

--- 03_generate_rnn_data.py
+++ 03_generate_rnn_data.py
@@ -20,6 +20,8 @@ K.set_session(sess)
 
 def main(args):
     obs_folder = args.obs_folder
+    #Checking that input folder exists and is not empty.
+    assert(os.path.exists(obs_folder) and len(os.listdir(obs_folder)) != 0)
     obs_filename_base = 'obs_data_doomrnn_'
     actions_filename_base = 'action_data_doomrnn_'
     obs_file_pattern = os.path.join(obs_folder,obs_filename_base+'*')
@@ -39,6 +41,8 @@ def main(args):
       print(vae_weights, " does not exist - ensure you have run 02_train_vae.py first")
       raise
 
+    #KOEChange Modified here to avoid memory problems when working with large files.
+    #Now, we load 1 file at a time.
     obs_data = []
     action_data = []
     for file_number in range(1,len(glob.glob(obs_file_pattern))+1):
@@ -50,42 +54,40 @@ def main(args):
         print("loading action file ", action_file)
         for episode in np.load(action_file):
             action_data.append(episode)
-    print("-----LOADING FILES DONE -------")
 
-    obs_data = np.array(obs_data)
-    action_data = np.array(action_data)
+        obs_data = np.array(obs_data)
+        action_data = np.array(action_data)
+        print("Obs data has shape ", obs_data.shape)
+        print("action data has shape ", action_data.shape)
 
-    print("Obs data has shape ", obs_data.shape)
-    print("action data has shape ", action_data.shape)
+        #If the image data is integral (range [0, 255]), we convert to float in range [0,1]
+        #print(obs_data)
+        converted_obs_data=[]
+        if np.issubdtype(obs_data[0][0].dtype, np.integer):
+            print("Image data was of integer type. Converting to float before further processing.")
+            for episode in obs_data:
+                converted_episode = []
+                for image in episode:
+                    converted_image = image.astype('float32') / 255.
+                    converted_episode.append(converted_image)
+                converted_obs_data.append(converted_episode)
+            obs_data = converted_obs_data
 
-    #If the image data is integral (range [0, 255]), we convert to float in range [0,1]
-    #print(obs_data)
-    converted_obs_data=[]
-    if np.issubdtype(obs_data[0][0].dtype, np.integer):
-        print("Image data was of integer type. Converting to float before further processing.")
-        for episode in obs_data:
-            converted_episode = []
-            for image in episode:
-                converted_image = image.astype('float32') / 255.
-                converted_episode.append(converted_image)
-            converted_obs_data.append(converted_episode)
-        obs_data = converted_obs_data
+        #TODO Can we wait with storing the z-sequences until the end, or will that fill up memory?
+        #Need to store each ep separately. we cant predict btw episodes
+        #TODO Note: There are equally many actions and observations. I guess the final action can just be discarded?
+        z_sequences = [] #One for each ep
+        action_sequences = [] #One for each ep
+        for episode_number in range(len(obs_data)):
+            observations = np.array(obs_data[episode_number])
+            # Generating all latent codes for this episode
+            latent_values = vae.generate_latent_variables(observations)
+            z_sequences.append(latent_values)
+            action_sequences.append(np.array(action_data[episode_number]))
 
-    #Need to store each ep separately. we cant predict btw episodes
-    #TODO Note: There are equally many actions and observations. I guess the final action can just be discarded?
-    z_sequences = [] #One for each ep
-    action_sequences = [] #One for each ep
-    for episode_number in range(len(obs_data)):
-        observations = np.array(obs_data[episode_number])
-        # Generating all latent codes for this episode
-        latent_values = vae.generate_latent_variables(observations)
-        z_sequences.append(latent_values)
-        action_sequences.append(np.array(action_data[episode_number]))
-
-        print("Added latent sequences of length ", len(latent_values), " and action sequence of length ", len(action_sequences[-1]))
-        print("Array sizes: ", len(z_sequences), ", ", len(action_sequences))
-    z_sequences = np.array(z_sequences) #Will this work? Has sub-arrays of differing lengths.
-
+            print("Added latent sequences of length ", len(latent_values), " and action sequence of length ", len(action_sequences[-1]))
+            print("Array sizes: ", len(z_sequences), ", ", len(action_sequences))
+        z_sequences = np.array(z_sequences) #Will this work? Has sub-arrays of differing lengths.
 
     np.savez_compressed(os.path.join(savefolder, "rnn_training_data.npz"), action=action_data, latent = z_sequences)
 
@@ -97,8 +99,6 @@ if __name__ == "__main__":
                         default = "./data/")
     parser.add_argument('--loaded_vae_weights', type=str, help="Path to load VAE weights from.",
                         default = "./models/final_full_vae_weights.h5")
-    parser.add_argument('--action_file', type=str, help="Path to actions-file.",
-                        default = "./data/action_data_doomrnn_1.npy")
     parser.add_argument('--savefolder', type=str, help="Folder to store results in. Default is ./rnn-data/",
                         default = "./rnn-data/")
     args = parser.parse_args()
